74b2b4171651de938199685be0a440c4bd26a964
Tony Apuzzo <TonyApuzzo@yahoo.com>
2026-07-10 15:15:16 -0600

    Use yarn to upgrade packages.

---
+++ jest.config.cjs
@@ -0,0 +1,12 @@
+const { pathsToModuleNameMapper } = require('ts-jest');
+const { compilerOptions } = require('./tsconfig.json');
+
+module.exports = {
+  roots: ['<rootDir>/src'],
+  testMatch: ['**/__tests__/**/*.+(ts|tsx|js)', '**/?(*.)+(spec|test).+(ts|tsx|js)'],
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+  },
+  modulePaths: [compilerOptions.baseUrl],
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths),
+};
